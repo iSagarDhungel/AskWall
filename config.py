2b3d322211ed78b2d10de8d74981eea52a6f03c5
Sagar Dhungel <hiesagar@gmail.com>
2020-05-08 14:50:55 +0545

    feat: intrigate sqlite and add user table for login

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
+rootdir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
 	#app.config['SECRET_KEY'] = "I-am-Unguessable"
 	SECRET_KEY = os.environ.get('SECRET_KEY') or "I-am-Unguessable"
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///app.db' + os.path.join(rootdir, 'app.db')
+	# track changes in object
+	SQLALCHEMY_TRACK_MODIFICATION = False
