b13b4e04b883fb5dcbdd021f31dbf00e9e73a09e
Sagar <hiesagar@gmail.com>
2020-05-26 13:41:55 +0545

    fix: duplicate username while editprofile

--- app/forms.py
+++ app/forms.py
@@ -38,3 +38,14 @@ class EditProfileForm(FlaskForm):
 	about_me = TextAreaField('About me', validators=[Length(min=0, max=150)])
 	submit = SubmitField('Submit')
 
+	def __init__(self, original_username, *args, **kargs):
+		super(EditProfileForm, self).__init__(*args, **kargs)
+		self.original_username = original_username
+
+	def validate_username(self, username):
+		if username.data != self.original_username:
+			user = User.query.filter_by(username = username.data).first()
+			if user is not None:
+				raise ValidationError('Please use a different username.')
+
+
