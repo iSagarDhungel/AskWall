4e49f92b80b5ad376c74809a6f87e52e69633634
Sagar <hiesagar@gmail.com>
2020-05-26 19:12:46 +0545

    feat: show posts in home page from followed user

--- app/forms.py
+++ app/forms.py
@@ -49,3 +49,9 @@ class EditProfileForm(FlaskForm):
 				raise ValidationError('Please use a different username.')
 
 
+class PostForm(FlaskForm):
+	post = TextAreaField("Say Something", validators = [
+		DataRequired(), Length(min=1, max=140)])
+	submit= SubmitField('Submit')
+
+		
--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ from werkzeug.urls import url_parse
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
 
 
 @app.before_request
@@ -14,21 +14,20 @@ def before_request():
 		current_user.last_seen = datetime.utcnow()
 		db.session.commit()
  
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-	posts= [
-		{
-			'author': {'username':'Sagar'},
-			'body': 'Beautiful day in Kathmandu!'
-		},
-		{
-			'author': {'username': 'Sagar'},
-			'body': 'This is a good day'
-		}
-	]
-	return render_template('index.html', title = 'Home', posts=posts)
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(body=form.post.data, author=current_user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post is now live')
+		return redirect(url_for('index'))
+
+	posts = current_user.followed_posts().all()
+	return render_template('index.html', title = 'Home', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
